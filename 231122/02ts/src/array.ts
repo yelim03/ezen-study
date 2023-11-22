76272b20db9f0d51c97b67462c36574951d982c3
yelim03 <yelim5570@gmail.com>
2023-11-23 00:27:46 +0900

    231123

---
+++ 231122/02ts/src/array.ts
@@ -0,0 +1,10 @@
+// let array = new Array();
+// array.push(1);
+// array.push(2);
+// array.push(3);
+
+// console.log(array);
+
+let numArray: number[] = [1, 2, 3];
+let strArray: string[] = ["Hello", "World"];
+let personArray = [{ name: "jack" }, { name: "Jane", age: 32 }];
---
+++ 231122/02ts/src/split.ts
@@ -0,0 +1,3 @@
+//JS에서 split이라는 메서드는 어떤 역할을 할까
+//문자열을 배열로 변환(*간주)
+//merry christ mas => ["merry","chirst","mas"]
